01608f201739655041976355e2a0526a9fa6ba8e
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-10-28 03:00:58 +0530

    Tested for sample SD and variance

--- tests.py
+++ tests.py
@@ -12,5 +12,11 @@ print(f'Central tendencies of sample {sample}: \n {get_central(sample)}')
 print()
 
 # testing standard deviation
+
+## of population
 population = [2,4,21,32,423,12]
 print(f'For population {population}:\n Variance = {get_variance(population)} \n SD = {get_SD(population)}')
+
+## of sample
+sample = [2,12,423]
+print(f'For sample {sample}:\n {bessel_correction(sample)}')
