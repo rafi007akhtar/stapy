2315879029c60d53fa8477e4ea951a8e5a49fa58
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-10-28 03:30:17 +0530

    Tested for z-scores

--- tests.py
+++ tests.py
@@ -20,3 +20,13 @@ print(f'For population {population}:\n Variance = {get_variance(population)} \n 
 ## of sample
 sample = [2,12,423]
 print(f'For sample {sample}:\n {bessel_correction(sample)}')
+
+print()
+
+# testing standardization
+
+## z-scores
+sample = [205,137,20,90]
+print(f'For sample {sample} \n Z-scores: {get_Z_scores(sample, mean = 120, SD = 40)}')
+population = [99,102,95,110]
+print(f'For population {population} \n Z-scores: {get_Z_scores(population)}')
