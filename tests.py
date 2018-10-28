44c419d7ac7d6220516096d1fa2efddc26656329
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-10-28 17:31:22 +0530

    Tested for quartiles, IQR and outliers

--- tests.py
+++ tests.py
@@ -47,3 +47,33 @@ sample = [205,137,20,90]
 print(f'For sample {sample} \n Z-scores: {get_Z_scores(sample, mean = 120, SD = 40)}')
 population = [99,102,95,110]
 print(f'For population {population} \n Z-scores: {get_Z_scores(population)}')
+
+print()
+
+# testing variability
+
+## quartiles
+sample = [
+    38946, 43420, 49191, 50432, 50557, 
+    52580, 53595, 54135, 60181, 10000000
+]
+quartiles = get_quartiles(sample)
+print(f'For sample {sample} \n {quartiles} \n IQR = {get_IQR(distribution=None, quartiles=quartiles)}')
+
+sample = [1,2,3,4,5]
+quartiles = get_quartiles(sample)
+print(f'For sample {sample} \n {quartiles} \n IQR = {get_IQR(distribution=None, quartiles=quartiles)}')
+
+print()
+
+## outliers
+sample = [
+    38946, 43420, 49191, 50432, 50557, 
+    52580, 53595, 54135, 60181, 10000000
+]
+val = 10000000
+print(f"For sample {sample}")
+print (f" It is {is_outlier(val, sample)} that {val} is an outlier.")
+val = 60181
+print (f" It is {is_outlier(val, sample)} that {val} is an outlier.")
+
