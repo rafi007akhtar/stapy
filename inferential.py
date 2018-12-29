deb6f4e97c6e5ed41b952f1e2446fe093efaa2c9
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-12-29 21:09:27 +0530

    Wrote and tested functions for confidence intervals

--- inferential.py
+++ inferential.py
@@ -19,6 +19,33 @@ def error_margin_approx(z_star, sigma, n):
 
 	Returns
 	-------
-	> The approximate margin of error, given by z_star(sigma/n).
+	The approximate margin of error, given by z_star(sigma/root(n)).
 	"""
-	return z_star * (sigma / n)
+	return z_star * (sigma / (n ** 0.5))
+
+def get_confidence_interval(xbar, sigma, n, z_star):
+	"""
+	Get the confidence interval of the mean paramater of a distribution.
+
+	Parameters
+	----------
+	> xbar: mean of the sample
+	> sigma: standard deviation of the population
+	> n: size of the sample
+	> z_star: critical score of the confidence level
+
+	Returns
+	-------
+	A tuple denoting the range of values, both exclusive, for the mean of the population to lie in it.
+	"""
+
+	# calculate the approximate margin of error
+	error = error_margin_approx(z_star, sigma, n)
+	
+	low = xbar - error  # lower range val
+	up = xbar + error  # upper range val
+
+	return (low, up)
+
+
+
--- testI.py
+++ testI.py
@@ -1,6 +1,20 @@
 from inferential import *
 
+# testing estimation
+
+## approximate margin of error
 critical = 2
 SD = 16.04
 n = 35
 print(f"Margin of error for critical score {critical}, SD {SD} and size {n} is {error_margin_approx(critical, SD, n)} approximately")
+
+print()
+
+## confidence interval
+xbar = 40
+CI = get_confidence_interval(xbar, SD, n, critical)
+print(f"Confidence interval for this score: {CI}")
+
+critical = 1.96
+CI = get_confidence_interval(xbar, SD, n, critical)
+print(f"Exact confidence interval = {CI}")
