6acba40d8a24749624b9f012949a8c47a73f3dd0
Md Rafi Akhtar <alimdrafi@gmail.com>
2019-01-23 16:30:21 +0530

    Tested for H0, HA functions

--- testI.py
+++ testI.py
@@ -27,3 +27,21 @@ print(f"New confidence interval: {CI}")
 critical = 2.33
 CI = get_confidence_interval(xbar, SD, n, critical)
 print(f"Confidence interval for 98%: {CI}")
+
+print()
+
+# hypothesis testing
+
+## null hypothesis
+from descriptive import get_z_sample, get_SE
+n = 50
+xbar = 8.3
+mu = 7.47
+sigma = 2.41
+z = get_z_sample(xbar, mu, get_SE(sigma, n))
+z_star = 1.96
+
+if HA(mean_tuple = None, z_tuple=(z, z_star)):
+    print("Null is rejected")
+else:
+    print("Null is accepted")
